e197e4c6aa87c7d0a60c490a872a49eecb4f0dfb
AlexStryukov <strjukov00@gmail.com>
2020-09-17 06:53:59 +0300

    Added screen manager

--- App/main.py
+++ App/main.py
@@ -1,12 +1,21 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen, ScreenManager
 
-class Main_Box(BoxLayout):
+sm = ScreenManager()
+
+class MainBox(Screen):
     pass
+
+class LeftMenu(Screen):
+    pass
+
+sm.add_widget(MainBox())
+sm.add_widget(LeftMenu())
 
 class GTeacherApp(App):
     def build(self):
-        return Main_Box()
+        return sm
 
 GTeacherApp().run()
